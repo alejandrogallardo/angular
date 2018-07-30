8f7374edb9c9e114e32968c05387fb0eb709ed10
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2018-07-30 00:43:06 -0600

    cargando productos desde firebase

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {InfoPagina} from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+  equipo: any[] = [];
+
+  constructor( private http: HttpClient) {
+
+    // console.log('Servicio de info Pagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+  private cargarInfo(){
+    //  leer archivo json
+    this.http.get('assets/data/data-pagina.json')
+      .subscribe( (resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+      });
+  }
+  private cargarEquipo(){
+    this.http.get('https://angular-uno.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+        this.equipo = resp;
+        // console.log(resp);
+      });
+  }
+}
